8462bdf8c3486366e7aff62960c8531d78fe3156
Rohan3280 <rahul01sept@gmail.com>
2026-02-12 23:58:13 +0530

    Configuring Application Properties

--- src/main/java/com/codein/store/HomeController.java
+++ src/main/java/com/codein/store/HomeController.java
@@ -1,14 +1,19 @@
 package com.codein.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.swing.text.View;
+
 @Controller
 public class HomeController {
-    @RequestMapping("/")
-    public String Index()
-    {
-        return "Index.html";
-    }
+    @Value("${spring.application.name}")
+    private String appName;
 
+    @RequestMapping("/")
+    public String Index() {
+        System.out.println("app name : "+ appName);
+        return "index.html";
+    }
 }
