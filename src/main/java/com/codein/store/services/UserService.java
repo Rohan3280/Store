a6ae3fdf89c25a91282a7222f799c323a2991bf1
Rohan3280 <rahul01sept@gmail.com>
2026-03-02 23:50:13 +0530

    User Services

--- src/main/java/com/codein/store/services/UserService.java
+++ src/main/java/com/codein/store/services/UserService.java
@@ -1,4 +1,11 @@
 package com.codein.store.services;
 
+import com.codein.store.entities.User;
+
 public class UserService {
+    public void showEntityStates(){
+        var user = User.builder()
+                .name("John Doe")
+                .email
+    }
 }
